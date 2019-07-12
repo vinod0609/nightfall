8c7c411c75a4572b3667f0945440679692fb20d7
Liju Jose <liju.jose@gds.ey.com>
2019-07-12 14:43:09 +0530

    refactor(database): eslint fixes done
    refactor(database): mongo script which run as child-process moved to './business/accounts.service.js'

--- database/src/mongodb/db.js
+++ database/src/mongodb/db.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose')
-const { exec } = require('child_process');
+import { getProps } from '../config';
+import { COLLECTIONS } from '../common/constants';
+import {
+  UserSchema,
+  erc721Schema,
+  erc721TransactionSchema,
+  erc721CommitmentSchema,
+  erc721CommitmentTransactionSchema,
+  erc20TransactionSchema,
+  erc20CommitmentSchema,
+  erc20CommitmentTransactionSchema,
+} from '../models';
 
-let UserSchema = require('../models/user.model')
-let CoinSchema = require('../models/coin.model')
-let TokenSchema = require('../models/token.model')
-let TokenTransactionSchema = require('../models/token-transaction.model')
-let CoinTransactionSchema = require('../models/coin-transaction.model')
-let PublicTokenSchema = require('../models/public_token.model')
-let PublicTokenTransactionSchema = require('../models/public_token_transaction.model')
-let PublicCoinTransactionSchema = require('../models/public_coin_transaction.model')
+const { mongo } = getProps();
 
-const config = require('../config').getProps()
-
-module.exports = class DB {
+export default class DB {
   constructor (db, username) {
     this.database = db;
     this.username = username;
@@ -21,138 +22,162 @@ module.exports = class DB {
   }
 
   createTablesForUser () {
-    const {username, database} = this;
+    const { username, database } = this;
     this.Models = {
-      User:               database.model(`${username}_user`, UserSchema),
-      Coin:               database.model(`${username}_coin`, CoinSchema),
-      Token:              database.model(`${username}_token`, TokenSchema),
-      Token_Transaction:  database.model(`${username}_token_transaction`, TokenTransactionSchema),
-      Coin_Transaction:   database.model(`${username}_coin_transaction`, CoinTransactionSchema),
-      Public_Token:       database.model(`${username}_public_token`, PublicTokenSchema),
-      Public_Token_Transaction:       database.model(`${username}_public_token_transaction`, PublicTokenTransactionSchema),
-      Public_Coin_Transaction:       database.model(`${username}_public_coin_transaction`, PublicCoinTransactionSchema),
-    }
+      user: database.model(`${username}_${COLLECTIONS.USER}`, UserSchema),
+      erc_721: database.model(`${username}_${COLLECTIONS.PUBLIC_TOKEN}`, erc721Schema),
+      erc_721_transaction: database.model(
+        `${username}_${COLLECTIONS.PUBLIC_TOKEN_TRANSACTION}`,
+        erc721TransactionSchema,
+      ),
+      erc_721_commitment: database.model(
+        `${username}_${COLLECTIONS.TOKEN}`,
+        erc721CommitmentSchema,
+      ),
+      erc_721_commitment_transaction: database.model(
+        `${username}_${COLLECTIONS.TOKEN_TRANSACTION}`,
+        erc721CommitmentTransactionSchema,
+      ),
+      erc_20_transaction: database.model(
+        `${username}_${COLLECTIONS.PUBLIC_COIN_TRANSACTION}`,
+        erc20TransactionSchema,
+      ),
+      erc_20_commitment: database.model(`${username}_${COLLECTIONS.COIN}`, erc20CommitmentSchema),
+      erc_20_commitment_transaction: database.model(
+        `${username}_${COLLECTIONS.COIN_TRANSACTION}`,
+        erc20CommitmentTransactionSchema,
+      ),
+    };
   }
 
   async saveData (modelName, data) {
-    try{
-      let model = this.Models[modelName]
-      let modelInstance = new model(data);
-      let successData = await modelInstance.save();
-      return Promise.resolve(successData)
-    }catch(e){
-      return Promise.reject(e)
-    }
-  };
-
-
-  async getData (modelName, query = {}) {
-    try{
-      let model = this.Models[modelName]
-      let data = await model.find(query).exec();
-      return Promise.resolve(data)
-    } catch(e) {
-      console.log('DB error', e);
-      return Promise.reject(e)
+    try {
+      const Model = this.Models[modelName];
+      const modelInstance = new Model(data);
+      const successData = await modelInstance.save();
+      return Promise.resolve(successData);
+    } catch (e) {
+      return Promise.reject(e);
     }
   }
 
-  async getDbData (modelName, query, projection = {path: '', select: ''}, sort = {}, pageNo = 1, limit = 5) {
-    try{
-      console.log('pageNo', pageNo, 'limit', limit);
-      if (!Number(pageNo)) pageNo = 1;
-      if(!projection) projection = {path: '', select: ''};
-      let model = this.Models[modelName]
-      let data = await model.find(query)
-      .limit(limit)
-      .skip(limit*(pageNo-1))
-      .sort(sort)
-      .populate(projection)
-      .exec();
-      let totalCount = await model.find(query).countDocuments().exec();
-      return Promise.resolve({data: data, totalCount: totalCount});
-    } catch(e){
-      return Promise.reject(e)
+  async getData (modelName, query = {}) {
+    try {
+      const model = this.Models[modelName];
+      const data = await model.find(query).exec();
+      return Promise.resolve(data);
+    } catch (e) {
+      console.log('DB error', e);
+      return Promise.reject(e);
+    }
+  }
+
+  async getDbData (
+    modelName,
+    query,
+    projection = { path: '', select: '' },
+    sort = {},
+    pageNo = 1,
+    limit = 5,
+  ) {
+    try {
+      const model = this.Models[modelName];
+      const data = await model
+        .find(query)
+        .limit(limit)
+        .skip(limit * (pageNo - 1))
+        .sort(sort)
+        .populate(projection)
+        .exec();
+      const totalCount = await model
+        .find(query)
+        .countDocuments()
+        .exec();
+      return Promise.resolve({ data, totalCount });
+    } catch (e) {
+      return Promise.reject(e);
     }
   }
 
   async getDbValues (modelName, query, projection, sort = {}, pageNo, limit) {
-    try{
-      let model = this.Models[modelName]
-      let mQuery = model.find(query);
-      if(limit){
+    try {
+      const model = this.Models[modelName];
+      const mQuery = model.find(query);
+      if (limit) {
         mQuery.limit(limit);
       }
-      if(pageNo){
-        mQuery.skip(limit*(pageNo-1));
+      if (pageNo) {
+        mQuery.skip(limit * (pageNo - 1));
       }
-      if(sort){
+      if (sort) {
         mQuery.sort(sort);
       }
-      if(projection){
+      if (projection) {
         mQuery.populate(projection);
       }
 
-      let data = await mQuery.exec();
-      return Promise.resolve({data: data});
-    } catch(e){
-      return Promise.reject(e)
+      const data = await mQuery.exec();
+      return Promise.resolve({ data });
+    } catch (e) {
+      return Promise.reject(e);
     }
   }
 
   async findOne (modelName, query) {
     try {
-      let model = this.Models[modelName]
-      let data = await model.findOne(query)
-      return Promise.resolve(data)
-    }catch(e){
-      return Promise.reject(e)
+      const model = this.Models[modelName];
+      const data = await model.findOne(query);
+      return Promise.resolve(data);
+    } catch (e) {
+      return Promise.reject(e);
     }
   }
 
   async getListData (modelName, query, page) {
-    try{
-      let model = this.Models[modelName]
-      let data = await model.find(query).skip(page.index*page.size).limit(page.size).exec()
-      return Promise.resolve(data)
-    }catch(e){
-      return Promise.reject(e)
+    try {
+      const model = this.Models[modelName];
+      const data = await model
+        .find(query)
+        .skip(page.index * page.size)
+        .limit(page.size)
+        .exec();
+      return Promise.resolve(data);
+    } catch (e) {
+      return Promise.reject(e);
     }
   }
 
-  async updateData (modelName,condition,updateData, options = {upsert: true}) {
-    try{
-      let model = this.Models[modelName]
-      let data = await model.updateOne(condition, updateData, options)
-      return Promise.resolve(data)
-    }catch(e){
-      return Promise.reject(e)
+  async updateData (modelName, condition, updateData, options = { upsert: true }) {
+    try {
+      const model = this.Models[modelName];
+      const data = await model.updateOne(condition, updateData, options);
+      return Promise.resolve(data);
+    } catch (e) {
+      return Promise.reject(e);
     }
   }
 
   async aggregation (modelName, condition, projection, options) {
-    try{
-      let model = this.Models[modelName]
-      const pipeline = [{ "$match": condition }];
+    try {
+      const model = this.Models[modelName];
+      const pipeline = [{ $match: condition }];
 
-      if (projection)
-      pipeline.push(projection);
+      if (projection) pipeline.push(projection);
 
-      if (options)
-      pipeline.push(options);
+      if (options) pipeline.push(options);
 
-      let data = await model.aggregate(pipeline);
+      const data = await model.aggregate(pipeline);
 
-      return Promise.resolve(data)
-    } catch(e) {
-      return Promise.reject(e)
+      return Promise.resolve(data);
+    } catch (e) {
+      return Promise.reject(e);
     }
   }
 
   async populate (modelName, data, populates) {
     try {
-      let model = this.Models[modelName];
-    return await model.populate(data, populates);
+      const model = this.Models[modelName];
+      return await model.populate(data, populates);
     } catch (e) {
       return Promise.reject(e);
     }
@@ -164,27 +189,18 @@ module.exports = class DB {
         name,
         password,
         {
-          roles: [{
-            role: "read",
-            db: config.mongo.databaseName
-          }]
+          roles: [
+            {
+              role: 'read',
+              db: mongo.databaseName,
+            },
+          ],
         },
-        (err, result) => {
+        function (err) {
           if (err) return reject(err);
-          resolve();
-        }
-      );
-    })
-  }
-
-  updateUserRole () {
-    return new Promise((resolve, reject) => {
-      exec(`mongo nightfall --host=mongo -u ${config.mongo.admin} -p ${config.mongo.password} script_to_configure_roles.js`,
-        (err) => {
-          if (err) return reject(err);
-          resolve();
-        }
+          return resolve();
+        },
       );
     });
   }
-};
+}
