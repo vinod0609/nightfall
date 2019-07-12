cc45b5a32909e5d2700690ad56d876a5ed49f82b
Liju Jose <liju.jose@gds.ey.com>
2019-07-12 14:41:02 +0530

    refactor(datbase): ES6 import/export used
    refactor(database): admin config for key name password changed to adminPassword

--- database/src/config.js
+++ database/src/config.js
@@ -4,43 +4,35 @@
 @desc constants used by a nubmer of other modules
 */
 
-let env = 'local' //set the environment to local if not mentioned while starting the app
+let env = 'local'; // set the environment to local if not mentioned while starting the app
 const props = {
   local: {
     app: {
       host: 'http://database',
-      port: "80"
+      port: '80',
     },
     mongo: {
       host: 'mongo',
-      port: "27017",
-      databaseName: "nightfall",
-      admin: "admin",
-      password: "admin"
+      port: '27017',
+      databaseName: 'nightfall',
+      admin: 'admin',
+      adminPassword: 'admin',
     },
-    enable_logger: true,
-    crypt_secret: "secret" // used for encrypt and dercypt user other accounts password. [used in utils/crypto.js]
+    isLoggerEnable: true,
   },
-}
+};
 
 /**
  * Set the environment
  * @param { string } environment - environment of app
  */
-let setEnv = (environment) => {
-    if(props[environment]){
-        env = environment
-    }
+export function setEnv (environment) {
+  if (props[environment]) {
+    env = environment;
+  }
 }
 
 /**
  * get the appropriate environment config
  */
-let getProps = () => {
-    return props[env]
-}
-
-module.exports = {
-    setEnv,
-    getProps
-}
+export const getProps = () => props[env];
