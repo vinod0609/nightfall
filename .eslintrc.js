72d50d130a7f2712d872f025a7dd72064caecc28
Liju Jose <liju.jose@gds.ey.com>
2019-07-12 14:48:13 +0530

    feat(eslint): setting up lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
     'no-console': 'off',
     'space-before-function-paren': ['error', 'always'],
     'no-use-before-define': 'off',
+    'radix': 'off',
+    // 'multiline-ternary': ['error', 'never'],
     // 'prettier/prettier': 'off',   // make prettier config off
     'quotes': ['error', 'single'],
     'comma-dangle': ['error', 'always-multiline'],
