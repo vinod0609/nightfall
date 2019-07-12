ba0d356d72181076d3bfb81b6549706fc48e0176
Liju Jose <liju.jose@gds.ey.com>
2019-07-12 14:53:11 +0530

    refactor(database): converted json file to js as ES6 import/export cannot handle json files

---
+++ database/src/common/constants.js
@@ -0,0 +1,11 @@
+
+export const COLLECTIONS = {
+  USER: 'user',
+  PUBLIC_TOKEN: 'erc_721',
+  PUBLIC_TOKEN_TRANSACTION: 'erc_721_transaction',
+  TOKEN: 'erc_721_commitment',
+  TOKEN_TRANSACTION: 'erc_721_commitment_transaction',
+  PUBLIC_COIN_TRANSACTION: 'erc_20_transaction',
+  COIN: 'erc_20_commitment',
+  COIN_TRANSACTION: 'erc_20_commitment_transaction',   
+};
