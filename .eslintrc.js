c38ac1154c03f6f51d4cb333089a746d1a0b62e5
Liju Jose <liju.jose@gds.ey.com>
2019-07-15 22:46:54 +0530

    refactor(eslint): keeping preitter off for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,8 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'space-before-function-paren': ['error', 'always'],
-    'no-use-before-define': 'off',
     'radix': 'off',
-    // 'multiline-ternary': ['error', 'never'],
-    // 'prettier/prettier': 'off',   // make prettier config off
+    'prettier/prettier': 'off',   // make prettier config off
     'quotes': ['error', 'single'],
     'comma-dangle': ['error', 'always-multiline'],
     'semi': 'error',
