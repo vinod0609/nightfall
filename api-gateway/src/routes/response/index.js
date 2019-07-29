2785e4c5fb5806b419c8926b504431ae3bada314
Liju Jose <liju.jose@gds.ey.com>
2019-07-29 17:01:16 +0530

    refactor(eslint): couldn't use underscore dangle - eslint rule

--- api-gateway/src/routes/response/index.js
+++ api-gateway/src/routes/response/index.js
@@ -1,16 +1,16 @@
 export default class Response {
   set statusCode(code) {
-    this._statusCode = code;
+    this.code = code;
   }
 
   set data(responseData) {
-    this._data = responseData;
+    this.responseData = responseData;
   }
 
   toJSON() {
-    return { 
-      statusCode: this._statusCode,
-      data: this._data,
+    return {
+      statusCode: this.code,
+      data: this.responseData,
     };
   }
 }
