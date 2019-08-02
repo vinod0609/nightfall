ff3c6bce10128ebf74dbbc4e9957e08b73badb44
Liju Jose <liju.jose@gds.ey.com>
2019-08-02 13:20:24 +0530

    refactor(accounts): now response is structured in a common middleware

--- accounts/src/app.js
+++ accounts/src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import account from './routes/account';
 import accounts from './routes/accounts';
+import { formatResponse, formatError, errorHandler } from './middlewares';
 
 const app = express();
 
@@ -14,7 +15,22 @@ app.use(bodyParser.json());
 app.use('/account', account);
 app.use('/accounts', accounts);
 
-// handle bad calls
-app.use((req, res) => res.sendStatus(404));
+app.use(formatResponse);
+
+app.use(function logError(err, req, res, next) {
+  console.error(
+    `${req.method}:${req.url}
+    ${JSON.stringify({ error: err.message })}
+    ${JSON.stringify({ body: req.body })}
+    ${JSON.stringify({ params: req.params })}
+    ${JSON.stringify({ query: req.query })}
+  `,
+  );
+  console.error(JSON.stringify(err, null, 2));
+  next(err);
+});
+
+app.use(formatError);
+app.use(errorHandler);
 
 export default app;
--- accounts/src/routes/accounts.js
+++ accounts/src/routes/accounts.js
@@ -1,22 +1,19 @@
 /* eslint-disable func-names */
 
 import express from 'express';
-import Response from './response/response';
 import { unlockAccount } from '../services/accounts';
 
 const router = express.Router({ mergeParams: true });
 
-router.post('/unlock', async function(req, res) {
+router.post('/unlock', async function(req, res, next) {
   const { address, password } = req.body;
 
   try {
     await unlockAccount(address, password);
-
-    const response = new Response(200, { message: 'Unlocked' }, null);
-    res.json(response);
+    res.data = { message: 'Unlocked' };
+    next();
   } catch (err) {
-    const response = new Response(500, null, err);
-    res.status(500).json(response);
+    next(err);
   }
 });
 
