7b4197f7b01ad489454e151aafd81362f90cc81a
Will Kim <william.kim@ey.com>
2019-09-18 16:20:51 -0400

    refactor: refactor initial setup steps

--- zkp/code/index.js
+++ zkp/code/index.js
@@ -6,21 +6,11 @@ E.g. node src/tools-trusted-setup.js
 */
 
 import { argv } from 'yargs';
-import os from 'os';
 import fs from 'fs';
 import path from 'path';
 import inquirer from 'inquirer';
+import { compile, setup, exportVerifier } from '@eyblockchain/zokrates.js';
 
-import codePreProp from './tools-code-preprop';
-import keyExtractor from './tools-key-extractor';
-
-import Config from '../src/config';
-
-import zokrates from '../src/zokrates';
-
-const utils = require('../../zkp-utils/index.js'); // eslint-disable-line import/no-commonjs
-
-const config = Config.getProps();
 const isDirectory = source => fs.lstatSync(source).isDirectory();
 const getDirectories = source =>
   fs
@@ -45,293 +35,6 @@ if (!(a0 === undefined || a0 === '')) {
   a1 = null;
 }
 
-// s - suppress console streams
-const { s } = argv; // suppress - stream off if -s is specified
-
-// d - delete finished containers (WARNING: might delete containers for which the setup failed, so you wouldn't be able to go in and investigate the problem)
-const { d } = argv;
-
-/** create a promise that resolves to the output of a stream when the stream
-ends.  It also does some ZoKrates-specific error checking because not all 'errors'
-are supported on 'stderr'
-*/
-const promisifyStream = stream =>
-  new Promise((resolve, reject) => {
-    const MAX_RETURN = 10000000;
-    let chunk = '';
-    stream.on('data', dat => {
-      // chunk += d.toString("utf8").replace(/[^\x40-\x7F]/g, "").replace(/\0/g, '') //remove non-ascii, non alphanumeric
-      chunk += dat.toString('utf8'); // remove any characters that aren't in the proof.
-      if (chunk.length > MAX_RETURN) chunk = '...[truncacted]'; // don't send back too much stuff
-    });
-    stream.on('end', () => {
-      if (chunk.includes('panicked')) {
-        // errors thrown by the application are not always recognised
-        reject(new Error(chunk.slice(chunk.indexOf('panicked'))));
-      } else {
-        resolve(chunk);
-      }
-    });
-    stream.on('error', err => reject(err));
-  });
-
-async function getImportFiles(dataLines) {
-  const cpDataLines = [...dataLines];
-  return cpDataLines.reduce((accArr, line) => {
-    // parses each line of the .code file for a line of the form:
-    // import "./aux-adder.code" as ADD
-    //  and extracts "./aux-adder.code"
-    // ZoKrates' own packages will be ignored, as they are of the form:
-    // import "LIBSNARK/sha256compression"
-    //  which doesn't include ".code", and so are ignored.
-    line.replace(/((import ")+(.+\.+code+)+("+))+?/g, (m1, m2, ii, c) => {
-      if (c !== undefined) {
-        accArr.push(c);
-      }
-    });
-    return accArr;
-  }, []);
-}
-
-async function checkForImportFiles(codeFilePath, codeFileName, codeFileParentPath) {
-  console.log(`Checking for 'import' files in the .code file ${codeFileName}`);
-
-  const dataLines = await fs
-    .readFileSync(codeFilePath)
-    .toString('UTF8')
-    .split(os.EOL);
-
-  let importFiles = [];
-  importFiles = await getImportFiles(dataLines);
-  if (!(importFiles === undefined || importFiles.length === 0)) {
-    // array is nonempty
-    const len = importFiles.length;
-    for (let j = 0; i < len; j += 1) {
-      const file = importFiles[j];
-      try {
-        if (fs.existsSync(codeFileParentPath + file)) {
-          // file exists
-          console.log(`${file} is saved in the correct place`);
-        }
-      } catch (err) {
-        console.error(err);
-        console.error(`${file} not found in ${codeFileParentPath}`);
-      }
-    }
-  }
-}
-
-async function setup(codeFile, outputDirPath, backend, a) {
-  const codeFileName = codeFile.substring(0, codeFile.lastIndexOf('.'));
-
-  console.log(`codeFileName: ${codeFileName}`);
-  console.log(`codeFile: ${codeFile}`);
-  console.log(`outputDirPath: ${outputDirPath}`);
-  console.log(`backend: ${backend}`);
-  console.log(`a: ${a}`);
-
-  try {
-    container = await zokrates.runContainerMounted(outputDirPath);
-
-    await container;
-
-    console.log(`\nContainer running for ${codeFileName}`);
-    console.log(`Container id for ${codeFileName}`, `: ${container.id}`);
-    console.log(
-      `To connect to the ${codeFileName}`,
-      ` container manually: 'docker exec -ti ${container.id} bash'`,
-    );
-
-    console.group('\nCompile', codeFileName, '...');
-    // compile .code file
-    let output = await zokrates.compile(container, codeFile).catch(err => {
-      console.error(err);
-    });
-    if (!s) console.log(output);
-    console.log(codeFileName, 'SETUP MESSAGE: COMPILATION COMPLETE');
-    console.groupEnd();
-
-    // optionally run a check on an input string (if parameter -a is specified at runtime)
-    if (a) {
-      console.group('\nComputing witness', codeFileName, '...');
-      output = await zokrates.computeWitness(container, a).catch(err => {
-        console.error(err);
-      });
-      if (!s) console.log(output);
-      const lines = output.split(os.EOL);
-      const cpData = [...lines];
-      // ~out_130
-      let outVals = [];
-      const regex = /(~out_[0-9]+ )([0-9])+/g;
-      cpData
-        .filter(el => el.match(regex))
-        .map(el =>
-          el.replace(/(~out_[0-9]+ )([0-9])+/g, (m, out, _val) => {
-            const val = parseInt(_val, 10); // get the first number
-            outVals = outVals.concat(val);
-          }),
-        );
-      outVals = outVals.reverse(); // the outputs of zokrates are the wrong way around
-
-      if (!s) console.log(`outVals: \n${outVals}`);
-
-      let outVal;
-      switch (utils.isProbablyBinary(outVals)) {
-        case 'decimal':
-          console.group('\nOutput from compute-witness:\n');
-          console.log(`output array length: ${outVals.length}\n`);
-          console.log(`bin:  ${outVals.forEach(val => utils.decToBin(val))}`);
-          console.log(`dec:  ${outVals}`);
-          console.log(`hex:  ${outVals.forEach(val => utils.decToHex(val))}`);
-          console.groupEnd();
-          break;
-        default:
-          outVal = outVals.join('');
-          console.group('\nOutput from compute-witness:\n');
-          console.log(`output array length: ${outVals.length}\n`);
-          console.log(`bin:  ${outVal}`);
-          console.log(`dec:  ${utils.binToDec(outVal)}`);
-          console.log(`hex:  ${utils.binToHex(outVal)}`);
-          console.groupEnd();
-      }
-      console.log(codeFileName, 'SETUP MESSAGE: CREATE WITNESS COMPLETE');
-      console.groupEnd();
-    } else {
-      // the below runs only if arg '-a' is NOT specified.
-      // i.e. you can either compute a witness by specifying '-a', OR you can create a tar file...
-
-      // trusted setup to produce pk and vk
-      console.group('\nSetup', codeFileName, '...');
-      output = await zokrates.setup(container).catch(err => {
-        console.error(err);
-      });
-      if (!s) console.log(output);
-      console.log('SETUP MESSAGE: SETUP COMPLETE');
-      console.groupEnd();
-
-      // create a verifier.sol
-      console.group('\nExport Verifier', codeFileName, '...');
-      output = await zokrates.exportVerifier(container).catch(err => {
-        console.error(err);
-      });
-      if (!s) console.log(output);
-      console.log(codeFileName, 'SETUP MESSAGE: EXPORT-VERIFIER COMPLETE');
-      console.groupEnd();
-
-      // move the newly created files into your 'code' folder within the zokrates container.
-      const exec = await container.exec
-        .create({
-          Cmd: [
-            '/bin/bash',
-            '-c',
-            `cp ${
-              config.ZOKRATES_OUTPUTS_DIRPATH_ABS
-            }{out,out.code,proving.key,verification.key,variables.inf,verifier.sol} ${
-              config.ZOKRATES_CONTAINER_CODE_DIRPATH_ABS
-            }`,
-          ],
-          AttachStdout: true,
-          AttachStderr: true,
-        })
-        .catch(err => {
-          console.error(err);
-        });
-      output = await promisifyStream(await exec.start());
-      if (!s) console.log(output);
-      console.log(
-        codeFileName,
-        `SETUP MESSAGE: FILES COPIED TO THE MOUNTED DIR WITHIN THE CONTAINER. THE FILES WILL NOW ALSO EXIST WITHIN YOUR LOCALHOST'S FOLDER: ${outputDirPath}`,
-      );
-
-      console.group('\nKey extraction', codeFileName, '...');
-
-      // extract a JSON representation of the vk from the exported Verifier.sol contract.
-      const vkJSON = await keyExtractor.keyExtractor(`${outputDirPath}verifier.sol`, s);
-
-      if (vkJSON) {
-        fs.writeFileSync(`${outputDirPath + codeFileName}-vk.json`, vkJSON, function logErr(err) {
-          if (err) {
-            console.error(err);
-          }
-        });
-        console.log(`File: ${outputDirPath}${codeFileName}-vk.json created successfully`);
-      }
-      console.groupEnd();
-    }
-    if (!d) {
-      console.log(
-        `\nTo connect to the ${codeFileName} container manually: 'docker exec -ti ${
-          container.id
-        } bash'`,
-      );
-    } else {
-      await zokrates.killContainer(container);
-      console.log(`container ${container.id} killed`);
-    }
-
-    console.log(`${codeFileName} SETUP COMPLETE`);
-  } catch (err) {
-    console.log(err);
-    console.log(
-      '\n******************************************************************************************************************',
-      `\nTrusted setup has failed for ${codeFile}. Please see above for additional information relating to this error.`,
-      '\nThe most common cause of errors when using this tool is insufficient allocation of resources to Docker.',
-      "\nYou can go to Docker's settings and increase the RAM being allocated to Docker. See the README for more details.",
-      '\n******************************************************************************************************************',
-    );
-    return new Error(err);
-  }
-  return true; // looks like it worked. Return something for consistency
-}
-
-/**
-@param {string} codeFile A filename string of the form "my-code.code" or "my-code.pcode"
-@param {string} codeFileParentPath The path to, but not including, the codeFile.
-@param {string} a OPTIONAL A string of arguments, separated by space characters only.
-*/
-async function filingChecks(codeFile, codeFileParentPath) {
-  /**
-  codeFilePath: e.g. "./code/my-code.code"
-  codeFile: e.g. "my-code.code"
-  codeFileName: e.g. "my-code"
-  codeFileExt: e.g. "code"
-  */
-  console.log(`\nFiling checks for codeFile: ${codeFile}`);
-  // check we're working with either a .code or a .pcode file.
-  const codeFileName = await codeFile.substring(0, codeFile.lastIndexOf('.'));
-  const codeFileExt = await codeFile.substring(codeFile.lastIndexOf('.') + 1, codeFile.length);
-  if (!(codeFileExt === 'code' || codeFileExt === 'pcode')) {
-    return new Error("Invalid file extenstion. Expected a '.code' or a '.pcode' file.");
-  }
-  const codeFilePath = codeFileParentPath + codeFile;
-  let pwd = process.cwd();
-  pwd += '/code/';
-  // For .pcode files, create the .code file, so that we may use it in the container. The newly created codeFile is saved in codeFileParentPath dir by the codePreProp function.
-  if (codeFileExt === 'pcode') {
-    // let's copy the .pcode file into the safe_dump dir in case things go wrong; so we don't lose our original file.
-    try {
-      await fs.copyFileSync(codeFilePath, `${pwd}safe-dump/${codeFile}`);
-      console.log(`${codeFilePath} was copied to safe-dump/${codeFile}`);
-    } catch (err) {
-      return new Error(err);
-    }
-    await codePreProp.preProp1(codeFilePath, codeFileName, codeFileParentPath);
-  } else {
-    // .code file has been specified, but let's copy it into the safe_dump dir in case things go wrong; so we don't lose our original file.
-    try {
-      await fs.copyFileSync(codeFilePath, `${pwd}safe-dump/${codeFile}`);
-      console.log(`${codeFilePath} was copied to safe-dump/${codeFile}`);
-    } catch (err) {
-      return new Error(err);
-    }
-  }
-
-  await checkForImportFiles(codeFilePath, codeFileName, codeFileParentPath);
-
-  console.groupEnd();
-  return codeFileName;
-}
-
 function readdirAsync(_path) {
   return new Promise(function prm(resolve, reject) {
     fs.readdir(_path, function rdr(error, result) {
@@ -344,162 +47,69 @@ function readdirAsync(_path) {
   });
 }
 
-async function checkForOldFiles(dir) {
-  const files = await readdirAsync(dir);
+/**
+ * Calls Zokrates compile, setup, and export-verifier on a single directory
+ * @param {String} directoryPath
+ */
+async function generateZokratesFiles(directoryPath) {
+  const files = await readdirAsync(directoryPath);
 
-  console.log('\n\nFound existing files:', files, 'in', dir);
-  console.log(
-    "\n\nIf you continue, these files will be deleted (except for the '.pcode' file and any '.code' dependencies).",
+  const directoryWithSlash = directoryPath.endsWith('/') ? directoryPath : `${directoryPath}/`;
+
+  let codeFile;
+  // Look for a .code file that's not out.code. That's the file we're compiling.
+  for (let i = 0; i < files.length; i += 1) {
+    if (files[i].endsWith('.code') && files[i] !== 'out.code') {
+      codeFile = files[i];
+      break;
+    }
+  }
+
+  console.log('Compiling at', `${directoryWithSlash}${codeFile}`);
+
+  // Generate out.code and out in the same directory.
+  await compile(`${directoryWithSlash}${codeFile}`, directoryWithSlash);
+
+  console.log('Finished compiling at', directoryPath);
+
+  // Generate verification.key and proving.key
+  await setup(
+    `${directoryWithSlash}out`,
+    directoryWithSlash,
+    'gm17',
+    'verification.key',
+    'proving.key',
   );
 
-  const carryOn = await inquirer.prompt([
-    {
-      type: 'skip',
-      name: 'skip',
-      message: 'Continue with the trusted setup? y/n ',
-      choices: ['y', 'n'],
-    },
-  ]);
-  if (carryOn.skip !== 'y') return false;
+  console.log('Finished setup at', directoryPath);
 
-  return files;
+  await exportVerifier(
+    `${directoryWithSlash}/verification.key`,
+    directoryWithSlash,
+    'verifier.sol',
+    'gm17',
+  );
+
+  console.log('Finished export-verifier at', directoryPath);
 }
 
-async function rmOldFiles(dir, files) {
-  for (let j = 1; j < files.length; j += 1) {
-    const filePrefix = files[j].substring(0, 3);
-    const fileExt = files[j].substring(files[j].lastIndexOf('.') + 1, files[j].length);
-    if (!(fileExt === 'pcode' || filePrefix === 'aux')) {
-      console.log('deleting', files[j]);
-      fs.unlink(path.join(dir, files[j]), err => {
-        if (err) throw err;
-      });
-    }
-  }
-  const remainingFiles = await readdirAsync(dir);
-  console.log('\nFiles remaining:', remainingFiles, 'in', dir);
-}
-
-// RUN
-async function runSetup(a) {
-  // check we're parsing the correct directory:
-  console.group('Checking pwd...');
-  let pwd = process.cwd();
-  console.log(`pwd: ${pwd}`);
-  const pwdName = pwd.substring(pwd.lastIndexOf('/') + 1, pwd.length);
-  console.log(`pwdName: ${pwdName}`);
-  if (pwdName !== config.ZKP_PWD) {
-    throw new Error(`Wrong PWD. Please call this executable file from: ${config.ZKP_PWD}`);
-  }
-  pwd += '/code/';
-  console.groupEnd();
-
-  const dir = pwd + i;
-  console.log(`directory: ${dir}`);
-
-  let backend;
-  if (i.indexOf('pghr13') >= 0) {
-    backend = 'pghr13'; // NOTE: although this tool supports PGHR13, the wider Nightfall opensource repo does not support pghr13.
-  } else if (i.indexOf('gm17') >= 0) {
-    backend = 'gm17';
-  } else {
-    throw new Error("Incorrect backend or folder specified. Expected either 'pghr13' or 'gm17'.");
-  }
-
-  let files = await checkForOldFiles(dir);
-  if (files === false) {
-    throw new Error('user cancelled the setup');
-  }
-  await rmOldFiles(dir, files);
-
-  files = await readdirAsync(dir);
-
-  // filter all files for ones with extension .code or .pcode
-  files = files.filter(f => {
-    const codeFileExt = f.substring(f.lastIndexOf('.') + 1, f.length);
-    const codeFilePrefix = f.substring(0, 3);
-    if (codeFileExt !== 'pcode' || codeFilePrefix === 'aux') {
-      return false;
-    }
-    return true;
-  });
-
-  for (let j = 0; j < files.length; j += 1) {
-    const codeFile = files[j];
-    const codeFileParentPath = `${dir}/`;
-    const codeFileName = await filingChecks(codeFile, codeFileParentPath, a); // eslint-disable-line no-await-in-loop
-    await setup(`${codeFileName}.code`, codeFileParentPath, backend, a); // eslint-disable-line no-await-in-loop
-  }
-}
-
+/**
+ * Calls zokrates' compile, setup, and export-verifier on all directories in `/zkp/code/gm17`.
+ * @param {*} a
+ */
 async function runSetupAll(a) {
-  // check we're parsing the correct directory:
-  console.group('Checking pwd...');
-  let pwd = process.cwd();
-  console.log(`pwd: ${pwd}`);
-  const pwdName = pwd.substring(pwd.lastIndexOf('/') + 1, pwd.length);
-  console.log(`pwdName: ${pwdName}`);
-  if (pwdName !== config.ZKP_PWD) {
-    throw new Error(`Wrong PWD. Please call this executable file from: ${config.ZKP_PWD}`);
-  }
-  pwd += '/code/';
-  console.groupEnd();
+  // Directory that contains all the code directories.
+  const gm17Directory = `${process.cwd()}/code/gm17`;
+  // Array of directories.
+  const codeDirectories = getDirectories(gm17Directory);
 
-  let dirs = getDirectories(pwd);
-  console.log('\n\ndirs in', pwd, ':');
-  console.log(dirs);
+  await Promise.all(
+    codeDirectories.map(directory => {
+      return generateZokratesFiles(directory);
+    }),
+  );
 
-  // filter dirs to those of interest to us
-  dirs = dirs.filter(dir => {
-    const dirName = dir.substring(dir.lastIndexOf('/') + 1, dir.length);
-    if (dirName === 'gm17') {
-      return true;
-    }
-    return false;
-  });
-  console.log('\n\nrelevant dirs in', pwd, ':');
-  console.log(dirs);
-  // get the dirs within the dirs
-  const dirs2 = [];
-  // get the files within the dirs
-  for (let k = 0; k < dirs.length; k += 1) {
-    dirs2[k] = getDirectories(dirs[k]);
-    console.log('\n\ndirs in', dirs[k], ':');
-    console.log(dirs2[k]);
-
-    for (let l = 0; l < dirs2[k].length; l += 1) {
-      const dir = dirs[k];
-      const dir2 = dirs2[k][l];
-
-      let files = await checkForOldFiles(dir2); // eslint-disable-line no-await-in-loop
-      if (files !== []) {
-        await rmOldFiles(dir2, files); // eslint-disable-line no-await-in-loop
-        files = await readdirAsync(dir2); // eslint-disable-line no-await-in-loop
-        // filter all files for ones with extension .code or .pcode
-        files = files.filter(f => {
-          const codeFileExt = f.substring(f.lastIndexOf('.') + 1, f.length);
-          const codeFilePrefix = f.substring(0, 3);
-          if (codeFileExt !== 'pcode' || codeFilePrefix === 'aux') {
-            return false;
-          }
-          return true;
-        });
-        for (let j = 0; j < files.length; j += 1) {
-          const codeFile = files[j];
-          const codeFileParentPath = `${dir2}/`;
-          const backend = dir.substring(dir.lastIndexOf('/') + 1, dir.length);
-          const codeFileName = await filingChecks(codeFile, codeFileParentPath, a); // eslint-disable-line no-await-in-loop
-
-          try {
-            await setup(`${codeFileName}.code`, codeFileParentPath, backend, a); // eslint-disable-line no-await-in-loop
-          } catch (err) {
-            console.log(err);
-            break;
-          }
-        }
-      }
-    }
-  }
+  console.log('done');
 }
 
 async function allOrOne() {
